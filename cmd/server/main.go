b52774297b6ca28bf772c11eb8cf9703496ab0e9
roman <rom.ch.egod@gmail.com>
2025-08-19 12:44:16 +0300

    my hui commit

---
+++ cmd/server/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	createFile()
+	readfile()
+}
+
+func createFile() {
+	content := "ЖРИ СВОЙ ЕБАНЫЙ ФАЙЛ, кстати да, это из файла тебе привет"
+	err := os.WriteFile("data.txt", []byte(content), 0644)
+	if err != nil {
+		fmt.Printf("Ошибка создания: %v\n", err)
+		return
+	}
+}
+
+func readfile() {
+	data, err := os.ReadFile("data.txt")
+	if err != nil {
+		fmt.Printf("АХТУНГ БЛЯДЬ: %v\n", err)
+		return
+	}
+	fmt.Printf("держи бля: %s\n", data)
+}

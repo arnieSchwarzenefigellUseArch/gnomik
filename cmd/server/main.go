e45970cb2e0145ee6f390489470660a4a0d5bdba
roman <rom.ch.egod@gmail.com>
2025-09-03 11:17:25 +0300

    my http

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,35 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"os"
-)
-
 func main() {
-	createFile()
-	readfile()
-}
 
-func createFile() {
-<<<<<<< HEAD
-	content := "ЖРИ СВОЙ ЕБАНЫЙ ФАЙЛ, кстати да, это из файла тебе привет и кстати опять привет папа гыгыгыгыг, не ругайся" &&
-=======
-
-
-	content := "не ругайся"
->>>>>>> 6c591b899b3cd968824486a491ab86d6c674f5a2
-	err := os.WriteFile("data.txt", []byte(content), 0644)
-	if err != nil {
-		fmt.Printf("Ошибка создания: %v\n", err)
-		return
-	}
-}
-
-func readfile() {
-	data, err := os.ReadFile("data.txt")
-	if err != nil {
-		fmt.Printf("АХТУНГ БЛЯДЬ: %v\n", err)
-		return
-	}
-	fmt.Printf("держи бля: %s\n", data)
 }
